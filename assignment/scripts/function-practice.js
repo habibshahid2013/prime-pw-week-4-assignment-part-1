3bcc0e3636b2065fdec0ff78478261e85905b28c
Hassan Shahid <habibshahid2013@gmail.com>
2021-06-29 17:51:36 -0500

    Updated assignment part 1 with changes

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -55,15 +55,22 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 // 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
 function getLast( array ){
-  return array[ array.length - 1];
+  return array[array.length - 1];
 }
-console.log(getLast());
+console.log('Test the last item in the array', getLast(['money','candy']));
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
 function find( value, array ){
-
+  for (const x of array){
+    if ( x === value)
+  return true;
+  }
+  return false;
 }
+let array = ['trees', 'Leaves', 'wood', 'Roots']
+console.log('Here to test the true value of the function array', find('wood', array));// Here there is a loop that test to see if the function is true
+console.log('Seeing if I can get a false test of the function in a Array', find('moneyball', array));// Here there is a loop that test to see if the function is false
 
 // ----------------------
 // Stretch Goals
@@ -71,8 +78,12 @@ function find( value, array ){
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if ( letter === string[0]){
+    return true;
+  }
+    return false;
 }
+
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
